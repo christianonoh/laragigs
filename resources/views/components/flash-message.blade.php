dd53440a1d2ecf781af608581158a6140348030b
Christian Onoh <chibyk5000@gmail.com>
2024-03-08 09:54:32 +0100

    Add Alpine.js to make flash message dynamic

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,5 +1,5 @@
 @if (session()->has('message'))
-  <div class="fixed left-1/2 top-0 -translate-x-1/2 bg-laravel px-12 py-3 text-white">
+  <div x-data="{ open: true }" x-init="setTimeout(() => open = false, 3000)" x-show="open" class="fixed left-1/2 top-0 -translate-x-1/2 bg-laravel px-12 py-3 text-white">
     <p>{{ session('message') }}</p>
   </div>
 @endif
